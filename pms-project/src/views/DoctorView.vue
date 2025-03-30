3ba0069c65235dda59d6a533389efafe4d4bc18b
Kirk Besa <kirkpatrickbesa@gmail.com>
2025-03-30 20:41:43 +0800

    Initial Commit

---
+++ pms-project/src/views/DoctorView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>{{ title }}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      title: 'Patients'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.value)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
