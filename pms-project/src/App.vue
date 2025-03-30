f96928360dc5dd6c5f4a3226e5805f725a6d5fd9
Kirk Besa <kirkpatrickbesa@gmail.com>
2025-03-30 21:25:17 +0800

    global styles and variable fonts

--- pms-project/src/App.vue
+++ pms-project/src/App.vue
@@ -3,5 +3,6 @@
     <router-link to="/">Login</router-link> |
     <router-link to="/register">Register</router-link>
   </nav>
+
   <router-view/>
 </template>
--- pms-project/src/components/auth/Register.vue
+++ pms-project/src/components/auth/Register.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
         <h1>Register</h1>
+        <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+        </form>
     </div>
 </template>
-
 
 <script>
 export default {
