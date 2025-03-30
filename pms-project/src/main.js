8ed247e210fb128167c8198b7f974e74632faa9f
Kirk Besa <kirkpatrickbesa@gmail.com>
2025-03-31 02:11:28 +0800

    sidebar nav design

--- pms-project/src/main.js
+++ pms-project/src/main.js
@@ -4,5 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import '@/assets/styles/global.css'
 import '@/assets/styles/utilities.css'
+import '@/assets/styles/styles.css'
 
 createApp(App).use(router).mount('#app')
--- pms-project/src/router/doctor_routes.js
+++ pms-project/src/router/doctor_routes.js
@@ -1,13 +1,13 @@
-import Dashboard from '@/views/Doctor/Dashboard.vue';
+import DoctorDashboard from '@/views/Doctor/Dashboard.vue';
 import AddPatient from '@/views/Doctor/AddPatient.vue';
 import ViewAppointments from '@/views/Doctor/ViewAppointments.vue';
 import ViewPatients from '@/views/Doctor/ViewPatients.vue';
 
 const doctor_routes = [
   {
-    path: '',
+    path: 'DoctorDashboard',
     name: 'DoctorDashboard',
-    component: Dashboard,
+    component: DoctorDashboard,
   },
   {
     path: 'ViewPatients',
