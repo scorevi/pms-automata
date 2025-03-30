a34fe4625f2a0c40e915ad2bb84b0b1d656b579f
kirkbesa <kirk.besa@ciit.edu.ph>
2025-03-30 21:25:17 +0800

    global styles and variable fonts

--- pms-project/src/components/auth/Login.vue
+++ pms-project/src/components/auth/Login.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1>Login</h1>
+        <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+        </form>
     </div>
 </template>
 
