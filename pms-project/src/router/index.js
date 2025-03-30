fb0d453e8455ad37d9f8f5b8a01c20b095148e67
kirkbesa <kirk.besa@ciit.edu.ph>
2025-03-30 23:04:59 +0800

    login and register form

--- pms-project/src/router/index.js
+++ pms-project/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/components/auth/Login.vue'
 import Register from '@/components/auth/Register.vue'
+import DoctorView from '@/views/DoctorView.vue'
+import PatientView from '@/views/PatientView.vue'
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/register',
     name: 'register',
     component: Register
+  },
+  {
+    path: '/doctor',
+    name: 'doctor',
+    component: DoctorView
+  },
+  {
+    path: '/patient',
+    name: 'patient',
+    component: PatientView
   }
 ]
 
