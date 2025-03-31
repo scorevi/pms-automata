9f484111adc6ae8bc82bc7ce1ba741c0b9d213b0
iverocs <sean.caintic@ciit.edu.ph>
2025-03-31 11:05:26 +0800

    added logout function

--- pms-project/src/views/DoctorView.vue
+++ pms-project/src/views/DoctorView.vue
@@ -2,10 +2,22 @@
   <div class="user-view-container flex-row">
     <div class="side-bar-container">
       <nav class="nav flex-column">
-        <router-link to="/DoctorView/DoctorDashboard"><span class="material-symbols-outlined">dashboard</span>Dashboard</router-link>
-        <router-link to="/DoctorView/ViewPatients"><span class="material-symbols-outlined">patient_list</span>View Patients</router-link>
-        <router-link to="/DoctorView/ViewAppointments"><span class="material-symbols-outlined">calendar_month</span>View Appointments</router-link>
-        <router-link to="/DoctorView/AddPatient"><span class="material-symbols-outlined">person_add</span>Add Patient</router-link>
+        <router-link to="/DoctorView/DoctorDashboard">
+          <span class="material-symbols-outlined">dashboard</span>Dashboard
+        </router-link>
+        <router-link to="/DoctorView/ViewPatients">
+          <span class="material-symbols-outlined">patient_list</span>View Patients
+        </router-link>
+        <router-link to="/DoctorView/ViewAppointments">
+          <span class="material-symbols-outlined">calendar_month</span>View Appointments
+        </router-link>
+        <router-link to="/DoctorView/AddPatient">
+          <span class="material-symbols-outlined">person_add</span>Add Patient
+        </router-link>
+        <!-- Replace router-link with an anchor that calls logout -->
+        <a href="#" @click.prevent="logout">
+          <span class="material-symbols-outlined">logout</span>Log Out
+        </a>
       </nav>
     </div>
     <div class="content-container">
@@ -15,7 +27,25 @@
 </template>
 
 <script>
+import { getAuth, signOut } from "firebase/auth";
+import Cookies from "js-cookie";
+
 export default {
   name: "DoctorView",
+  methods: {
+    logout() {
+      const auth = getAuth();
+      signOut(auth)
+        .then(() => {
+          Cookies.remove("loggedIn");
+          Cookies.remove("lastActivity");
+          Cookies.remove("userRole");
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          console.error("Error during logout:", error);
+        });
+    },
+  },
 };
 </script>
